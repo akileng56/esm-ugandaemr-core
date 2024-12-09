1ba480dd1297a1f7d397574cbccedc39bfca5ebf
Isaac Akileng <isaacakileng56@gmail.com>
2024-12-09 16:04:11 +0300

    Remove dev link

--- packages/esm-ugandaemr-app/src/pages/clinical-patient-summary/clincial-patient-summary.ts
+++ packages/esm-ugandaemr-app/src/pages/clinical-patient-summary/clincial-patient-summary.ts
@@ -1,3 +1,9 @@
+/*
+ * TODO
+ * Use a dynamic url for the SHR Profile
+ * Use a patient Identifier as per the SHR
+ * Handle other encounters other than vitals
+ * */
 export async function getClinicalData() {
   let apiUrl = `https://hapi.devearea.com/hapi-fhir-jpaserver/fhir//Patient?identifier=10000X&_include=Patient:link&_revinclude=Encounter:patient&_revinclude=Observation:patient`;
 
